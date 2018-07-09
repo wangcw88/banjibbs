00e058b126975ae8d48b101d83f8fe6a84100070
wangcw88 <351648839@qq.com>
2018-07-09 23:36:16 +0800

    Add a test file

--- src/test/java/com/dianziban/bbs/Test.java
+++ src/test/java/com/dianziban/bbs/Test.java
@@ -11,24 +11,6 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class Test{
-
-    public void aaa() {
-        for(int i =0;i<4;i++) {
-            for (int j = 0; j < 5; j++) {
-                if (j == 0) {
-                    System.out.println("ok");
-                } else if (i == 0) {
-                    System.out.println("false");
-                }
-                else {
-                    System.out.println((int)Math.pow(2,2));
-
-                }
-            }
-        }
-
-    }
-
     @org.junit.Test
     public void show(){
         List<Integer> squareList = generateSquareList(100);
@@ -47,9 +29,4 @@ public class Test{
         }
         return squareList;
     }
-
-
-
-
-
 }
